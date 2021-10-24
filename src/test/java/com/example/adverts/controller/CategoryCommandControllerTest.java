b4c87ed4810eb7bc3b8e98f3727ed793bd5bd2aa
Francislainy Campos <francislainy.campos@hmhco.com>
2021-10-24 04:09:57 +0100

    Add test for update category

--- src/test/java/com/example/adverts/controller/CategoryCommandControllerTest.java
+++ src/test/java/com/example/adverts/controller/CategoryCommandControllerTest.java
@@ -2,6 +2,7 @@ package com.example.adverts.controller;
 
 import com.example.adverts.controller.category.CategoryCommandController;
 import com.example.adverts.model.dto.category.CategoryCreateDto;
+import com.example.adverts.model.dto.category.CategoryUpdateDto;
 import com.example.adverts.service.interfaces.category.CategoryCommandService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
@@ -18,6 +19,8 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(CategoryCommandController.class)
@@ -54,6 +57,38 @@ public class CategoryCommandControllerTest {
                 .andExpect(content().json(jsonResponse, true))
                 .andExpect(jsonPath("$.id").value("2da4002a-31c5-4cc7-9b92-cbf0db998c41"))
                 .andExpect(jsonPath("$.title").value("category"))
+                .andReturn();
+
+        System.out.println(mvcResult.getResponse().getContentAsString());
+    }
+
+
+    @Test
+    public void testUpdateCategory() throws Exception {
+
+        CategoryUpdateDto categoryUpdateDto = new CategoryUpdateDto();
+        categoryUpdateDto.setTitle("updated category");
+        CategoryUpdateDto categoryUpdateResponseDto = new CategoryUpdateDto(UUID.fromString("2da4002a-31c5-4cc7-9b92-cbf0db998c41"), "updated category");
+
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        String jsonUpdateBody = ow.writeValueAsString(categoryUpdateDto);
+        String jsonResponse = ow.writeValueAsString(categoryUpdateResponseDto);
+
+        Mockito.when(categoryCommandService.updateCategory(any(UUID.class), eq(categoryUpdateDto))).thenReturn(
+                categoryUpdateResponseDto);
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .put("/api/adverts/category/2da4002a-31c5-4cc7-9b92-cbf0db998c41")
+                .content(jsonUpdateBody)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON);
+        mockMvc.perform(request).andReturn();
+
+        MvcResult mvcResult = mockMvc.perform(request)
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().json(jsonResponse, true))
+                .andExpect(jsonPath("$.id").value("2da4002a-31c5-4cc7-9b92-cbf0db998c41"))
+                .andExpect(jsonPath("$.title").value("updated category"))
                 .andReturn();
 
         System.out.println(mvcResult.getResponse().getContentAsString());
