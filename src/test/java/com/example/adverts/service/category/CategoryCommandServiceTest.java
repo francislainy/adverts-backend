d91a8cc7d2e31d32c3938fc4ad40f68ced8fe4eb
Francislainy Campos <francislainy.campos@hmhco.com>
2021-10-28 15:12:11 +0100

    Add test for subcategory service

--- src/test/java/com/example/adverts/service/category/CategoryCommandServiceTest.java
+++ src/test/java/com/example/adverts/service/category/CategoryCommandServiceTest.java
@@ -20,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-@WebMvcTest(CategoryQueryService.class)
+@WebMvcTest(CategoryCommandService.class)
 public class CategoryCommandServiceTest {
 
     @Mock
---
+++ src/test/java/com/example/adverts/service/subcategory/SubCategoryCommandServiceTest.java
@@ -0,0 +1,63 @@
+package com.example.adverts.service.subcategory;
+
+import com.example.adverts.model.dto.subcategory.SubCategoryCreateDto;
+import com.example.adverts.model.entity.category.Category;
+import com.example.adverts.model.entity.subcategory.SubCategory;
+import com.example.adverts.repository.category.CategoryRepository;
+import com.example.adverts.repository.subcategory.SubCategoryRepository;
+import com.example.adverts.service.impl.category.CategoryCommandImpl;
+import com.example.adverts.service.impl.category.CategoryQueryServiceImpl;
+import com.example.adverts.service.impl.subcategory.SubCategoryCommandImpl;
+import com.example.adverts.service.interfaces.category.CategoryCommandService;
+import com.example.adverts.service.interfaces.category.CategoryQueryService;
+import com.example.adverts.service.interfaces.subcategory.SubCategoryCommandService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@WebMvcTest(SubCategoryCommandService.class)
+public class SubCategoryCommandServiceTest {
+
+    @Mock
+    CategoryRepository categoryRepository;
+
+    @Mock
+    SubCategoryRepository subCategoryRepository;
+
+    @MockBean
+    private SubCategoryCommandService subCategoryCommandService;
+
+    @BeforeEach
+    void initUseCase() {
+        subCategoryCommandService = new SubCategoryCommandImpl(categoryRepository, subCategoryRepository);
+    }
+
+    @Test
+    public void testSubCategoryItemSavedToDb() {
+        Category category = new Category();
+        category.setId(UUID.fromString("02c903f7-7a55-470d-8449-cf7587f5a3fb"));
+
+        SubCategory subCategoryMocked = new SubCategory(UUID.fromString("03c903f7-7a55-470d-8449-cf7587f5a3fb"), "subcategory", category);
+
+        when(subCategoryRepository.save(any(SubCategory.class))).thenReturn(subCategoryMocked);
+        when(categoryRepository.findById(any(UUID.class))).thenReturn(java.util.Optional.of(category));
+
+        SubCategoryCreateDto subCategoryCreateDto = new SubCategoryCreateDto("subcategory", UUID.fromString("03c903f7-7a55-470d-8449-cf7587f5a3fb"));
+
+        subCategoryCreateDto = subCategoryCommandService.createSubCategory(subCategoryCreateDto, UUID.fromString("02c903f7-7a55-470d-8449-cf7587f5a3fb"));
+
+        assertNotNull(subCategoryCreateDto);
+        assertEquals(subCategoryMocked.getId(), subCategoryCreateDto.getId());
+        assertEquals(subCategoryMocked.getTitle(), subCategoryCreateDto.getTitle());
+    }
+
+}
