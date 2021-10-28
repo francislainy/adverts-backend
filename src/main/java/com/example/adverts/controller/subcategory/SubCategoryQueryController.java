e524f53dbb679d30845b84a29aac9efb20449275
Francislainy Campos <francislainy.campos@hmhco.com>
2021-10-28 16:37:20 +0100

    Add get subcategory and list

---
+++ src/main/java/com/example/adverts/controller/subcategory/SubCategoryQueryController.java
@@ -0,0 +1,39 @@
+package com.example.adverts.controller.subcategory;
+
+import com.example.adverts.model.dto.subcategory.SubCategoryQueryDto;
+import com.example.adverts.service.interfaces.subcategory.SubCategoryQueryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@CrossOrigin
+@RequestMapping("/api/adverts/category/{id}/subcategory")
+@RestController
+public class SubCategoryQueryController {
+
+    @Autowired
+    private SubCategoryQueryService subCategoryQueryService;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, List<SubCategoryQueryDto>> listAllSubCategories() {
+
+        HashMap<String, List<SubCategoryQueryDto>> result = new HashMap<>();
+        result.put("subcategories", subCategoryQueryService.getAllSubCategories());
+        return result;
+
+    }
+
+    @GetMapping(value = "/{subCategoryId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<SubCategoryQueryDto> getCategory(@PathVariable(value = "subCategoryId") UUID id) {
+        return new ResponseEntity<>(subCategoryQueryService.getSubCategory(id), HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/example/adverts/service/interfaces/subcategory/SubCategoryQueryService.java
@@ -0,0 +1,16 @@
+package com.example.adverts.service.interfaces.subcategory;
+
+import com.example.adverts.model.dto.subcategory.SubCategoryQueryDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public interface SubCategoryQueryService {
+
+    SubCategoryQueryDto getSubCategory(UUID id);
+
+    List<SubCategoryQueryDto> getAllSubCategories();
+
+}
