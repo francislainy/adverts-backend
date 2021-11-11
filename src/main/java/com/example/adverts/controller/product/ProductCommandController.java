a045da23c963157a28f3a9ced77544f387facf5b
Francislainy Campos <francislainy.campos@hmhco.com>
2021-11-11 06:43:25 +0000

    Add error handler for product controller and tests

--- src/main/java/com/example/adverts/controller/product/ProductCommandController.java
+++ src/main/java/com/example/adverts/controller/product/ProductCommandController.java
@@ -1,5 +1,6 @@
 package com.example.adverts.controller.product;
 
+import com.example.adverts.model.FeedbackMessage;
 import com.example.adverts.model.dto.product.ProductCreateDto;
 import com.example.adverts.repository.category.CategoryRepository;
 import com.example.adverts.repository.subcategory.SubCategoryRepository;
@@ -27,12 +28,18 @@ public class ProductCommandController {
     private SubCategoryRepository subCategoryRepository;
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ProductCreateDto> createProduct(@RequestBody ProductCreateDto productCreateDto, @PathVariable UUID categoryId, @PathVariable UUID subCategoryId) {
+    public ResponseEntity<Object> createProduct(@RequestBody ProductCreateDto productCreateDto, @PathVariable UUID categoryId, @PathVariable UUID subCategoryId) {
         if (categoryRepository.existsById(categoryId) && subCategoryRepository.existsById(subCategoryId)) {
-            return new ResponseEntity<>(productCommandService.createProduct(productCreateDto, categoryId, subCategoryId), HttpStatus.CREATED);
+
+            if (productCreateDto.getTitle()!=null) {
+                return new ResponseEntity<>(productCommandService.createProduct(productCreateDto, categoryId, subCategoryId), HttpStatus.CREATED);
+            }
+            else {
+                return new ResponseEntity<>(new FeedbackMessage("Missing title"), HttpStatus.BAD_REQUEST);
+            }
         }
         else {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(new FeedbackMessage("Entity not found"), HttpStatus.BAD_REQUEST);
         }
     }
 
