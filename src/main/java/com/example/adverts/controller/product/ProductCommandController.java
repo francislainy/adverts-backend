4f9676ab5627f60e498510d149fc313f77019b2b
Francislainy Campos <francislainy.campos@hmhco.com>
2021-11-11 05:03:48 +0000

    Return bad request for product command controller when category or subcategory does not exist

--- src/main/java/com/example/adverts/controller/product/ProductCommandController.java
+++ src/main/java/com/example/adverts/controller/product/ProductCommandController.java
@@ -1,10 +1,9 @@
 package com.example.adverts.controller.product;
 
 import com.example.adverts.model.dto.product.ProductCreateDto;
-import com.example.adverts.model.dto.subcategory.SubCategoryCreateDto;
-import com.example.adverts.model.dto.subcategory.SubCategoryUpdateDto;
+import com.example.adverts.repository.category.CategoryRepository;
+import com.example.adverts.repository.subcategory.SubCategoryRepository;
 import com.example.adverts.service.interfaces.product.ProductCommandService;
-import com.example.adverts.service.interfaces.subcategory.SubCategoryCommandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -21,10 +20,20 @@ public class ProductCommandController {
     @Autowired
     private ProductCommandService productCommandService;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private SubCategoryRepository subCategoryRepository;
+
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<ProductCreateDto> createProduct(@RequestBody ProductCreateDto productCreateDto, @PathVariable UUID categoryId, @PathVariable UUID subCategoryId) {
-        return new ResponseEntity<>(productCommandService.createProduct(productCreateDto, categoryId, subCategoryId), HttpStatus.CREATED);
+        if (categoryRepository.existsById(categoryId) && subCategoryRepository.existsById(subCategoryId)) {
+            return new ResponseEntity<>(productCommandService.createProduct(productCreateDto, categoryId, subCategoryId), HttpStatus.CREATED);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
 }
