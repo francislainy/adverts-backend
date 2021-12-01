21d9637b82c0efd46ee1538b755b4ed373badcda
Francislainy Campos <francislainy.campos@hmhco.com>
2021-12-01 07:07:00 +0000

    Remove validation from user controller body as this is now done under the dto class

--- src/main/java/com/example/adverts/controller/user/UserCommandController.java
+++ src/main/java/com/example/adverts/controller/user/UserCommandController.java
@@ -1,6 +1,5 @@
 package com.example.adverts.controller.user;
 
-import com.example.adverts.model.FeedbackMessage;
 import com.example.adverts.model.dto.user.UserCreateDto;
 import com.example.adverts.service.interfaces.user.UserCommandService;
 import com.example.adverts.util.CustomExceptionHandler;
@@ -23,12 +22,7 @@ public class UserCommandController extends CustomExceptionHandler {
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> createUser(@Valid @RequestBody UserCreateDto userCreateDto) {
 
-        if (userCreateDto.getEmail() != null && userCreateDto.getUsername() != null && userCreateDto.getPassword() != null) {
-            return new ResponseEntity<>(userCommandService.createUser(userCreateDto), HttpStatus.CREATED);
-        } else {
-            return new ResponseEntity<>(new FeedbackMessage("Missing mandatory field"), HttpStatus.BAD_REQUEST);
-        }
-
+        return new ResponseEntity<>(userCommandService.createUser(userCreateDto), HttpStatus.CREATED);
     }
 
 }
